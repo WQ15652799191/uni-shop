380f222391dee3e1ce9321a96fea1afd49f342de
wuqing <m15652799191@163.com>
2022-05-19 14:43:33 +0800

    订单支付

---
+++ src/components/my-login/my-login.vue
@@ -0,0 +1,124 @@
+<template>
+    <view class="login-container">
+        <!-- 提示登录的图标 -->
+        <uni-icons type="contact-filled" size="100" color="#AFAFAF"></uni-icons>
+        <!-- 登录按钮 -->
+        <button type="primary" class="btn-login" @click="getUserProfile">一键登录</button>
+        <!-- <button type="primary" class="btn-login" open-type="getUserInfo" @getuserinfo="getUserInfo">一键登录</button> -->
+        <!-- 登录提示 -->
+        <view class="tips-text">登录后尽享更多权益</view>
+    </view>
+</template>
+
+<script>
+import { mapMutations, mapState } from 'vuex'
+export default {
+    methods: {
+        ...mapMutations('m_user', ['updateUserInfo', 'updateToken', 'updateRedirectInfo']),
+        // getUserInfo(e) {
+        //     if (e.detail.errMsg === 'getUserInfo:fail auth deny') return uni.$showMsg('您取消了登录授权！');
+        //     this.updateUserInfo(e.detail.userInfo)
+        //     // 获取登录成功后的 Token 字符串
+        //     this.getToken(e.detail)
+        // },
+        // 调用登录接口，换取永久的 token
+        async getToken(info) {
+            // 调用微信登录接口
+            const [err, res] = await uni.login().catch(err => err)
+            console.log(res);
+            // 判断是否 uni.login() 调用失败
+            if (err || res.errMsg !== 'login:ok') return uni.$showError('登录失败！')
+
+            // 准备参数对象
+            const query = {
+                code: res.code,
+                encryptedData: info.encryptedData,
+                iv: info.iv,
+                rawData: info.rawData,
+                signature: info.signature
+            }
+
+            // 换取 token
+            const { data: loginResult } = await uni.$http.post('/api/public/v1/users/wxlogin', query)
+            console.log(loginResult);
+            // if (loginResult.meta.status !== 200) return uni.$showMsg('登录失败！')
+            uni.$showMsg('登录成功');
+            // this.updateToken(loginResult.message.token);
+            this.updateToken("wqwqwqwwqwq");
+            this.navigateBack()
+        },
+        getUserProfile() {
+            uni.getUserProfile({
+                desc: "你的授权信息",
+                success: res => {
+                    this.updateUserInfo(res.userInfo)
+                    this.getToken(res)
+                    console.log(res);
+                },
+                fail: res => {
+                    return uni.$showMsg("取消了登录授权")
+                }
+            })
+        },
+        navigateBack() {
+            // redirectInfo 不为 null，并且导航方式为 switchTab
+            if (this.redirectInfo && this.redirectInfo.openType === 'switchTab') {
+                // 调用小程序提供的 uni.switchTab() API 进行页面的导航
+                uni.switchTab({
+                // 要导航到的页面地址
+                url: this.redirectInfo.from,
+                // 导航成功之后，把 vuex 中的 redirectInfo 对象重置为 null
+                complete: () => {
+                    this.updateRedirectInfo(null)
+                }
+                })
+            }
+        }
+    },
+    computed: {
+        ...mapState('m_user', ["redirectInfo"])
+    }
+}
+</script>
+
+<style lang="scss">
+.login-container {
+  // 登录盒子的样式
+  height: 750rpx;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #f8f8f8;
+  position: relative;
+  overflow: hidden;
+
+  // 绘制登录盒子底部的半椭圆造型
+  &::after {
+    content: ' ';
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 40px;
+    left: 0;
+    bottom: 0;
+    background-color: white;
+    border-radius: 100%;
+    transform: translateY(50%);
+  }
+
+  // 登录按钮的样式
+  .btn-login {
+    width: 90%;
+    border-radius: 100px;
+    margin: 15px 0;
+    background-color: #c00000;
+  }
+
+  // 按钮下方提示消息的样式
+  .tips-text {
+    font-size: 12px;
+    color: gray;
+  }
+}
+</style>
--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,13 +1,34 @@
 <template>
-    <div>
-        my
-    </div>
+    <view class="my-container">
+        <!-- 用户未登录时，显示登录组件 -->
+        <my-login v-if="!token"></my-login>
+
+        <!-- 用户登录后，显示用户信息组件 -->
+        <my-userinfo v-else></my-userinfo>
+    </view>
 </template>
 
 <script>
+import MyLogin from "../../components/my-login/my-login.vue";
+import MyUserinfo from "../../components/my-userinfo/my-userinfo.vue";
+import { mapState } from 'vuex'
 import badgeMix from '../../mixins/tabbar-badge'
 export default {
     // 将 badgeMix 混入到当前的页面中进行使用
     mixins: [badgeMix],
+    components: {
+        MyLogin,
+        MyUserinfo
+    },
+    
+    computed: {
+        ...mapState('m_user', ['token'])
+    }
 }
 </script>
+<style lang="scss">
+page,
+.my-container {
+  height: 100%;
+}
+</style>
